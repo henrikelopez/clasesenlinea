2268d1431d6c1e198391e3f7887d238eaee7bdaa
Proyectos <Proyectos@sistemas-hk>
2013-07-11 17:05:24 -0500

    Traducciones

--- application/views/nosotros.php
+++ application/views/nosotros.php
@@ -1,182 +1,182 @@
 <div class="top-row">
-            	<h1 class="aligncenter">Nuestra experiencia es la base <br>para su innovación.</h1>
-                <div class="wrapper">
-                	<div class="grid_5 prefix_1 alpha">
-                    	<div class="wrapper">
-                        	<span class="dropcap color-1">A.</span>
-                            <div class="extra-wrap">
-                            	<strong class="str-1 color-1">
-                                	Recursos <br>Nosotros le proveemos el recurso necesario para comenzar a gestionar sus clases en línea.
-                                </strong>
-                                <a href="#" class="link">ver más</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="grid_5">
-                    	<span class="dropcap color-2">B.</span>
-                            <div class="extra-wrap">
-                            <strong class="str-1 color-2">
-                                Maestros <br>Proveemos el recurso mas utilizado y conocido en este ambiente, es decir moodle, cualquier duda o sugerecia estamos a sus ordenes. 
-                            </strong>
-                            <a href="#" class="link">ver más</a>
-                        </div>
-                    </div>
-                    <div class="grid_5">
-                    	<span class="dropcap color-3">C.</span>
-                        <div class="extra-wrap">
-                            <strong class="str-1 color-3">
-                                Alumnos <br>Los mas beneficiados, este espacio :) queremos ofrecerles un servicio que exploten para su aprendizaje, sean bienvenidos. 
-                            </strong>
-                            <a href="#" class="link">ver más</a>
-                        </div>
-                    </div>
-                    <div class="grid_7 suffix_1 omega">
-                    	<span class="dropcap color-4">D.</span>
-                        <div class="extra-wrap">
-                            <strong class="str-1 color-4">
-                                Solicitar el recurso es totalmente gratuito, por el momento si lo requiere escribanos a contacto@clasesenlinea.mx
-                            </strong>
-                            <a href="#" class="link">ver más</a>
-                        </div>
-                    </div>
+    <h1 class="aligncenter">Nuestra experiencia es la base <br>para su innovación.</h1>
+    <div class="wrapper">
+        <div class="grid_5 prefix_1 alpha">
+            <div class="wrapper">
+                <span class="dropcap color-1">A.</span>
+                <div class="extra-wrap">
+                    <strong class="str-1 color-1">
+                        Recursos <br>Nosotros le proveemos el recurso necesario para comenzar a gestionar sus clases en línea.
+                    </strong>
+                    <a href="#" class="link">ver más</a>
                 </div>
             </div>
-            <div class="wrapper indent-top">
-            	<article class="grid_23 prefix_1 alpha omega">
-                    <h2>Meet our team:</h2>
-                    <div class="wrapper">
-                        <div class="grid_7 suffix_1 alpha">
-                        	<figure class="img-indent">
-                            	<img src="images/page2-img1.jpg" alt="">
-                            </figure>
-                            <div class="extra-wrap">
-                            	<p class="p1">Vivamus hendrerit mauris ut dui gravida ut viverra lectus tincidunt. Cras mattis tempor</p>
-                                <div class="author">
-                                	Sara Green
-									<em>manager</em>
-                                </div>
-                            </div>
-                            <div class="clear"></div>
-                        </div>
-                        <div class="grid_7 suffix_1">
-                        	<figure class="img-indent">
-                            	<img src="images/page2-img2.jpg" alt="">
-                            </figure>
-                            <div class="extra-wrap">
-                            	<p class="p1">Vivamus hendrerit mauris gravida ut viverra lectus tincidunt. Cas matis tempor eros nec.</p>
-                                <div class="author">
-                                	Richard Smith
-									<em>pr director</em>
-                                </div>
-                            </div>
-                            <div class="clear"></div>
-                        </div>
-                        <div class="grid_7 omega">
-                        	<div class="pad-3">
-                                <figure class="img-indent">
-                                    <img src="images/page2-img3.jpg" alt="">
-                                </figure>
-                                <div class="extra-wrap">
-                                    <p class="p1">Vivamus hendrerit mauris ut dui gravida ut viverra lectus tincidunt.</p>
-                                    <div class="author">
-                                        April Nelson
-                                        <em>marketolog</em>
-                                    </div>
-                                </div>
-                                <div class="clear"></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="hr-border-1"></div>
-                    <div class="wrapper p">
-                        <div class="grid_7 suffix_1 alpha">
-                        	<figure class="img-indent">
-                            	<img src="images/page2-img4.jpg" alt="">
-                            </figure>
-                            <div class="extra-wrap">
-                            	<p class="p1">Vivamus hendrerit mauris ut dui gravida ut viverra lectus tincidunt. Cras mattis tempor</p>
-                                <div class="author">
-                                	John Johnson
-									<em>developer</em>
-                                </div>
-                            </div>
-                            <div class="clear"></div>
-                        </div>
-                        <div class="grid_7 suffix_1">
-                        	<figure class="img-indent">
-                            	<img src="images/page2-img5.jpg" alt="">
-                            </figure>
-                            <div class="extra-wrap">
-                            	<p class="p1">Vivamus hendrerit mauris gravida ut viverra lectus tincidunt. Cas matis tempor eros nec.</p>
-                                <div class="author">
-                                	Jessica Cox
-									<em>layer</em>
-                                </div>
-                            </div>
-                            <div class="clear"></div>
-                        </div>
-                        <div class="grid_7 omega">
-                        	<div class="pad-3">
-                                <figure class="img-indent">
-                                    <img src="images/page2-img6.jpg" alt="">
-                                </figure>
-                                <div class="extra-wrap">
-                                    <p class="p1">Vivamus hendrerit mauris ut dui gravida ut viverra lectus tincidunt.</p>
-                                    <div class="author">
-                                        Thom Black
-                                        <em>investor</em>
-                                    </div>
-                                </div>
-                                <div class="clear"></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="alignright pad-3">
-                        <a href="#" class="link">read more</a>
-                    </div>
-                </article>
+        </div>
+        <div class="grid_5">
+            <span class="dropcap color-2">B.</span>
+            <div class="extra-wrap">
+                <strong class="str-1 color-2">
+                    Maestros <br>Proveemos el recurso mas utilizado y conocido en este ambiente, es decir moodle, cualquier duda o sugerecia estamos a sus ordenes. 
+                </strong>
+                <a href="#" class="link">ver más</a>
             </div>
         </div>
-        <div class="clear"></div>
+        <div class="grid_5">
+            <span class="dropcap color-3">C.</span>
+            <div class="extra-wrap">
+                <strong class="str-1 color-3">
+                    Alumnos <br>Los mas beneficiados, este espacio :) queremos ofrecerles un servicio que exploten para su aprendizaje, sean bienvenidos. 
+                </strong>
+                <a href="#" class="link">ver más</a>
+            </div>
+        </div>
+        <div class="grid_7 suffix_1 omega">
+            <span class="dropcap color-4">D.</span>
+            <div class="extra-wrap">
+                <strong class="str-1 color-4">
+                    Solicitar el recurso es totalmente gratuito, por el momento si lo requiere escribanos a contacto@clasesenlinea.mx
+                </strong>
+                <a href="#" class="link">ver más</a>
+            </div>
+        </div>
     </div>
+</div>
+<div class="wrapper indent-top">
+    <article class="grid_23 prefix_1 alpha omega">
+        <h2>Nuestro Equipo:</h2>
+        <div class="wrapper">
+            <div class="grid_7 suffix_1 alpha">
+                <figure class="img-indent">
+                    <img src="images/page2-img1.jpg" alt="">
+                </figure>
+                <div class="extra-wrap">
+                    <p class="p1">Vacante</p>
+                    <div class="author">
+                        Sara Green
+                        <em>Dirección</em>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="grid_7 suffix_1">
+                <figure class="img-indent">
+                    <img src="images/page2-img2.jpg" alt="">
+                </figure>
+                <div class="extra-wrap">
+                    <p class="p1">Vacante.</p>
+                    <div class="author">
+                        Richard Smith
+                        <em>Sub Dirección</em>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="grid_7 omega">
+                <div class="pad-3">
+                    <figure class="img-indent">
+                        <img src="images/page2-img3.jpg" alt="">
+                    </figure>
+                    <div class="extra-wrap">
+                        <p class="p1">Ocupado.</p>
+                        <div class="author">
+                            April Nelson
+                            <em>Creatividad</em>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+        <div class="hr-border-1"></div>
+        <div class="wrapper p">
+            <div class="grid_7 suffix_1 alpha">
+                <figure class="img-indent">
+                    <img src="images/page2-img4.jpg" alt="">
+                </figure>
+                <div class="extra-wrap">
+                    <p class="p1">Ocupado</p>
+                    <div class="author">
+                        John Johnson
+                        <em>Informática</em>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="grid_7 suffix_1">
+                <figure class="img-indent">
+                    <img src="images/page2-img5.jpg" alt="">
+                </figure>
+                <div class="extra-wrap">
+                    <p class="p1">Vacante.</p>
+                    <div class="author">
+                        Jessica Cox
+                        <em>Comercialización</em>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+            <div class="grid_7 omega">
+                <div class="pad-3">
+                    <figure class="img-indent">
+                        <img src="images/page2-img6.jpg" alt="">
+                    </figure>
+                    <div class="extra-wrap">
+                        <p class="p1">Vivamus hendrerit mauris ut dui gravida ut viverra lectus tincidunt.</p>
+                        <div class="author">
+                            Thom Black
+                            <em>investor</em>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+        <div class="alignright pad-3">
+            <a href="#" class="link">ver más</a>
+        </div>
+    </article>
+</div>
+</div>
+<div class="clear"></div>
+</div>
 </section>
 <aside>
-	<div class="container_24">
-    	<div class="wrapper">
-        	<div class="grid_24 aside-bg">
-            	<div class="wrapper">
-                	<article class="grid_14 suffix_1 prefix_1 alpha">
-                    	<h2>Testimonials:</h2>
+    <div class="container_24">
+        <div class="wrapper">
+            <div class="grid_24 aside-bg">
+                <div class="wrapper">
+                    <article class="grid_14 suffix_1 prefix_1 alpha">
+                        <h2>Testimonios:</h2>
                         <div class="grid_7 alpha">
                             <blockquote class="quote">
                                 <div class="inner">
-                                    Vivamus hendrerit mauris ut dui gravida ut viverra lectus tincidunt. Cras mattis tempor eros nec tristique. Sed sed felis arcu, vel vehicula augue. Maecenas faucibus sagittis cursus.
+                                    ...próximamente, aún que si quieres ser el primero :D escribe a contacto@clasesenlinea.mx
                                 </div>
                             </blockquote>
                             <div class="author alignright pad-4">
-                                April Black <span>( manager )</span>
+                                Yo <span>( el productor )</span>
                             </div>
                         </div>
                         <div class="grid_7 omega">
-                        	<blockquote class="quote">
+                            <blockquote class="quote">
                                 <div class="inner">
-                                    Vivamus hendrerit mauris ut dui gravida ut viverra lectus tincidunt. Cras mattis tempor eros nec tristique. Sed sed felis arcu, vel vehicula augue. Maecenas faucibus sagittis cursus.
+                                    ...próximamente, aún que si quieres ser el primero :D escribe a contacto@clasesenlinea.mx
                                 </div>
                             </blockquote>
                             <div class="author alignright pad-4">
-                                Sandra Jobs <span>( manager )</span>
+                                Yo <span>( el productor )</span>
                             </div>
                         </div>
                         <div class="clear"></div>
                     </article>
                     <article class="grid_7 suffix_1 omega">
-                    	<h2>Features:</h2>
+                        <h2>Funciones:</h2>
                         <ol class="ol-list-1">
-                        	<li><a href="#">Vivamus hendrerit mauris ut dui</a></li> 
-                            <li><a href="#">gravida ut viverra lectus tincidunt</a></li>
-                            <li><a href="#">Cras mattis tempor. eros nec tristique</a></li> 
-                            <li><a href="#">Sed sed felis arcu, vel vehicula augue</a></li> 
-                            <li><a href="#">Maecenas faucibus sagittis cursus</a></li>
+                            <li><a href="#">LLegar a mas personas.</a></li>
+                            <li><a href="#">Aprender más.</a></li> 
+                            <li><a href="#">Enseñar más.</a></li> 
+                            <li><a href="#">No dejar caer el sitio.</a></li> 
+                            <li><a href="#">Conseguir dinero.</a></li>
                         </ol>
                     </article>
                 </div>
