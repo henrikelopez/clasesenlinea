052810484298c3371c1a35e78349d984c7a640c5
Proyectos <Proyectos@sistemas-hk>
2013-07-11 15:15:34 -0500

    Se modifico baseURL

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ if (!defined('BASEPATH'))
   | path to your installation.
   |
  */
-$config['base_url'] = 'http://clasesenlinea.mx';
+$config['base_url'] = 'http://clasesenlinea.mx/';
 
 /*
   |--------------------------------------------------------------------------
