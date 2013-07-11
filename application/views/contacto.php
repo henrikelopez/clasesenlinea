2763d7145044150d293242e73190ebcdc800663e
Proyectos <Proyectos@sistemas-hk>
2013-07-11 17:47:24 -0500

    Traducciones

--- application/views/contacto.php
+++ application/views/contacto.php
@@ -43,37 +43,35 @@
             </div>
             <div class="wrapper indent-top">
             	<article class="grid_7 suffix_1 prefix_1 alpha">
-                    <h2>Contact info:</h2>
+                    <h2>Estamos en:</h2>
                     <div class="map-container img-box">
                         <iframe src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Brooklyn,+New+York,+NY,+United+States&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=61.282355,146.513672&amp;ie=UTF8&amp;hq=&amp;hnear=Brooklyn,+Kings,+New+York&amp;ll=40.649974,-73.950005&amp;spn=0.01628,0.025663&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
                     </div>
-                    <strong class="str-4">Point</strong>
+                    <strong class="str-4">Visitanos</strong>
                     <dl class="adress">
-                        <dt>8901 Marmora Road, Glasgow, D04 89GR</dt>
-                        <dd><span>Telephone:</span><strong>+1 959 603 6035</strong></dd>
-                        <dd><span>Fax:</span><strong>+1 504 889 9898</strong></dd>
-                        <dd><span>Email:</span><a href="#">mail@demolink.org</a></dd>
+                        <dt>Guadalajara, Jalisco - México</dt>
+                        <dd><span>Email:</span><a href="#">contacto@clasesenlinea.mx</a></dd>
                     </dl>  
                 </article>
                 <article class="grid_14 suffix_1 omega">
-                    <h2>Contact form:</h2>
+                    <h2>Contactanos:</h2>
                     <form id="contact-form" method="post">
                       <fieldset>
                         <label class="name">
-                            <span>Full name:</span>
+                            <span>¿Cómo te llamas?</span>
                             <input type="text">
                         </label>
                         <label class="email">
-                          <span>Email:</span>
+                          <span>¿Cuál es tu email?</span>
                           <input type="email">
                         </label>
                         <label class="message">
-                          <span>Message:</span>	
+                          <span>Tu  mensaje:</span>	
                           <textarea></textarea>
                         </label>
                         <div class="btns">
-                            <a class="link" onClick="document.getElementById('contact-form').reset()">Clear</a>
-                            <a class="link" onClick="document.getElementById('contact-form').submit()">Send</a> 
+                            <a class="link" onClick="document.getElementById('contact-form').reset()">Nuevo</a>
+                            <a class="link" onClick="document.getElementById('contact-form').submit()">Enviar</a> 
                         </div>
                       </fieldset>
                     </form>
