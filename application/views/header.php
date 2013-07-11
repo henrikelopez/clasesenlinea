832382b977fd863a86e1d34c7409fb5c406165f9
Proyectos <Proyectos@sistemas-hk>
2013-07-11 16:02:29 -0500

    Codigo de seguimiento Google Analytics

--- application/views/header.php
+++ application/views/header.php
@@ -39,6 +39,23 @@
                 <script src="js/html5.js"></script>
                 <link rel="stylesheet" href="css/ie.css"> 
         <![endif]-->
+        <script type="text/javascript">
+
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-38926834-1']);
+            _gaq.push(['_setDomainName', 'clasesenlinea.mx']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script');
+                ga.type = 'text/javascript';
+                ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(ga, s);
+            })();
+
+        </script>
     </head>
     <!--<body id="page1">-->
     <body>
