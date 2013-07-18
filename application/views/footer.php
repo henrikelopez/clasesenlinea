d852cef40c3ee8b9a039fc914411bd35a944d305
Guillermo M Austin <guillermo.austin@gmail.com>
2013-07-17 19:10:38 -0500

    Ahora la pagina tiene adsense en footer y del lado izquierdo

--- application/views/footer.php
+++ application/views/footer.php
@@ -5,6 +5,29 @@
         <li><a href="#"><img src="images/social-link-2.jpg" alt=""></a></li>
         <li><a href="https://delicious.com/jius/search?p=clases"><img src="images/social-link-3.jpg" alt=""></a></li>
     </ul>
-</footer>
+ </footer>
+    <div style="margin:auto; width:970px; height:90px; padding-top:30px;>
+    <script type="text/javascript"><!--
+
+		google_ad_client = "ca-pub-5830654382485300";
+		
+		/* Footer en pagina */
+		
+		google_ad_slot = "5339854474";
+		
+		google_ad_width = 970;
+		
+		google_ad_height = 90;
+		
+		//-->
+		
+		</script>
+		
+		<script type="text/javascript"
+		
+		src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+		
+		</script>
+    </div>
 </body>
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -59,6 +59,30 @@
     </head>
     <!--<body id="page1">-->
     <body>
+    <div style="float:left; position:fixed; background-color:#CCC; width:120px; height:600px; top:15%; z-index:9800 !important;">
+        <script type="text/javascript"><!--
+
+google_ad_client = "ca-pub-5830654382485300";
+
+/* Flotador en página */
+
+google_ad_slot = "5619056074";
+
+google_ad_width = 120;
+
+google_ad_height = 600;
+
+//-->
+
+</script>
+
+<script type="text/javascript"
+
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+
+</script>
+</div>
+
         <!-- Header -->
         <header>
            <script type="text/javascript"><!--
